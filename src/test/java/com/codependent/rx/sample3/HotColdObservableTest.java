aa2886174974c1214340be5e4fcca52767bac01c
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2016-05-09 18:32:23 +0200

    New hot observable test with PublishSubject

--- src/test/java/com/codependent/rx/sample3/HotColdObservableTest.java
+++ src/test/java/com/codependent/rx/sample3/HotColdObservableTest.java
@@ -2,21 +2,21 @@ package com.codependent.rx.sample3;
 
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import com.codependent.rx.samplescada.machine.Signal;
-import com.codependent.rx.samplescada.machine.Machine.State;
 
 import rx.Observable;
 import rx.Observer;
 import rx.observables.ConnectableObservable;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
 @Test
 public class HotColdObservableTest {
 
+	@Test
 	public void testColdObservable() throws InterruptedException{
 		Observable<Integer> observable = Observable.range(1, 1000);
 		
@@ -32,6 +32,7 @@ public class HotColdObservableTest {
 		
 	}
 	
+	@Test
 	public void testHotObservable() throws InterruptedException{
 		
 		CountDownLatch latch = new CountDownLatch(1);
@@ -58,6 +59,7 @@ public class HotColdObservableTest {
 		
 	}
 	
+	@Test
 	public void testHotObservable2() throws InterruptedException{
 		
 		CountDownLatch latch = new CountDownLatch(1);
@@ -83,7 +85,8 @@ public class HotColdObservableTest {
 		
 	}
 	
-	public void testHotObservable23() throws InterruptedException{
+	@Test
+	public void testHotObservable3() throws InterruptedException{
 		
 		CountDownLatch latch = new CountDownLatch(1);
 		
@@ -124,6 +127,41 @@ public class HotColdObservableTest {
 		observable.connect();
 		
 		latch.await();
+	}
+	
+	@Test
+	public void testHotObservable4() throws InterruptedException{
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		
+		PublishSubject<Integer> subject = PublishSubject.<Integer>create();
+		
+		ConnectableObservable<Integer> series = Observable.<Object, Long, Integer>zip(
+				Observable.range(1, 10) , 
+				Observable.interval(1, TimeUnit.SECONDS),
+					(val, timer) -> (Integer)val)
+			    .doOnNext(item -> {
+			    	System.out.println("["+Thread.currentThread().getName()+ "] Emitting item: "+item);
+			    })
+			    .doOnCompleted( () -> latch.countDown()).publish();
+		
+		series.subscribe(subject);	
+		Thread.sleep(2000);
+		System.out.println("WAKE UP 1");
+		series.connect();
+		Thread.sleep(2000);
+		System.out.println("WAKE UP 2");
+		SensorReaderObserver sensor1 = new SensorReaderObserver();
+		subject.subscribe(sensor1);
+		Thread.sleep(4000);
+		System.out.println("WAKE UP 3");
+		SensorReaderObserver sensor2 = new SensorReaderObserver();
+		subject.subscribe(sensor2);
+		
+		latch.await();
+		
+		Assert.assertEquals(sensor1.getValues().size(), 8);
+		Assert.assertEquals(sensor2.getValues().size(), 4);
 	}
 	
 }
